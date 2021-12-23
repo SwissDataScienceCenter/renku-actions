d67188f7af5fef465c3ef86d6cb2b7e0089d190e
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2021-12-23 03:09:56 +0100

    chore: modify downloading of test artifacts

--- download-test-artifacts/entrypoint.sh
+++ download-test-artifacts/entrypoint.sh
@@ -4,6 +4,6 @@ export S3_HOST=$(yq '.tests.resultsS3.host' values.yaml -r)
 export S3_ACCESS=$(yq '.tests.resultsS3.accessKey' values.yaml -r)
 export S3_SECRET=$(yq '.tests.resultsS3.secretKey' values.yaml -r)
 export MC_HOST_bucket="https://${S3_ACCESS}:${S3_SECRET}@${S3_HOST}"
-mc cp bucket/dev-acceptance-tests-results/$TEST_ARTIFACTS_PATH.tgz .
-mc rm bucket/dev-acceptance-tests-results/$TEST_ARTIFACTS_PATH.tgz
-tar -C $GITHUB_WORKSPACE/ -xzvf $TEST_ARTIFACTS_PATH.tgz
+mkdir -p $GITHUB_WORKSPACE/test-artifacts
+mc cp --recursive bucket/dev-acceptance-tests-results/$TEST_ARTIFACTS_PATH/* $GITHUB_WORKSPACE/test-artifacts/
+mc rm --recursive --force bucket/dev-acceptance-tests-results/$TEST_ARTIFACTS_PATH
