13d3491ee2b0c371596c815136e5cb521df26ff3
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2022-01-12 21:10:03 +0100

    squashme: minor fix

--- download-test-artifacts/entrypoint.sh
+++ download-test-artifacts/entrypoint.sh
@@ -3,7 +3,10 @@ printf "%s" "$RENKU_VALUES" > values.yaml
 export S3_HOST=$(yq '.tests.resultsS3.host' values.yaml -r)
 export S3_ACCESS=$(yq '.tests.resultsS3.accessKey' values.yaml -r)
 export S3_SECRET=$(yq '.tests.resultsS3.secretKey' values.yaml -r)
+export S3_BUCKET=$(yq '.tests.resultsS3.bucket' values.yaml -r)
 export MC_HOST_bucket="https://${S3_ACCESS}:${S3_SECRET}@${S3_HOST}"
 mkdir -p $GITHUB_WORKSPACE/test-artifacts
-mc cp --recursive bucket/dev-acceptance-tests-results/$TEST_ARTIFACTS_PATH/* $GITHUB_WORKSPACE/test-artifacts/
-mc rm --recursive --force bucket/dev-acceptance-tests-results/$TEST_ARTIFACTS_PATH
+echo "Copying files from bucket/$S3_BUCKET/$TEST_ARTIFACTS_PATH/ to $GITHUB_WORKSPACE/test-artifacts/"
+mc cp --recursive bucket/$S3_BUCKET/$TEST_ARTIFACTS_PATH/* $GITHUB_WORKSPACE/test-artifacts/
+echo "Removing bucket/$S3_BUCKET/$TEST_ARTIFACTS_PATH"
+mc rm --recursive --force bucket/$S3_BUCKET/$TEST_ARTIFACTS_PATH
