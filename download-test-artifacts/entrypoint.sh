6766ddf1718f196b5733fe3b40a61ea6c4e3598d
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2022-01-12 22:14:30 +0100

    squashme: minor fix

--- download-test-artifacts/entrypoint.sh
+++ download-test-artifacts/entrypoint.sh
@@ -7,6 +7,6 @@ export S3_BUCKET=$(yq '.tests.resultsS3.bucket' values.yaml -r)
 export MC_HOST_bucket="https://${S3_ACCESS}:${S3_SECRET}@${S3_HOST}"
 mkdir -p $GITHUB_WORKSPACE/test-artifacts
 echo "Copying files from bucket/$S3_BUCKET/$TEST_ARTIFACTS_PATH/ to $GITHUB_WORKSPACE/test-artifacts/"
-mc cp --recursive bucket/$S3_BUCKET/$TEST_ARTIFACTS_PATH/* $GITHUB_WORKSPACE/test-artifacts/
+mc cp --recursive bucket/$S3_BUCKET/$TEST_ARTIFACTS_PATH/ $GITHUB_WORKSPACE/test-artifacts/
 echo "Removing bucket/$S3_BUCKET/$TEST_ARTIFACTS_PATH"
 mc rm --recursive --force bucket/$S3_BUCKET/$TEST_ARTIFACTS_PATH
