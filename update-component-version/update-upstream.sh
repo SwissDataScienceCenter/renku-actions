166a84e11d27cdd632e4a2048726d9f6ad584a8b
Pamela Delgado <pamela.delgado@epfl.ch>
2022-07-04 17:57:20 +0200

    fix: make script executable

--- update-component-version/update-upstream.sh
+++ update-component-version/update-upstream.sh
@@ -19,8 +19,8 @@ GIT_EMAIL=${GIT_EMAIL:=renku@datascience.ch}
 GIT_USER=${GIT_USER:="Renku Bot"}
 CHART_NAME=${CHART_NAME:=$(echo $GITHUB_REPOSITORY | cut -d/ -f2)}
 
-# build this chart to get the version
-chartpress --skip-build $CHART_TAG
+# get the chart version
+
 CHART_VERSION=$(yq r helm-chart/${CHART_NAME}/Chart.yaml version)
 
 git clone --depth=1 --branch=${UPSTREAM_BRANCH} https://${GITHUB_TOKEN}@github.com/${UPSTREAM_REPO} upstream-repo
