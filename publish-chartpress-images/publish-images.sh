73a5c8e04affc60cc6752e497e68b4d23ed06c92
Wes Johnson <wes@wes-johnson.com>
2025-03-25 08:49:13 +0100

    feat(publish-chartpress-images): support for providing platforms

--- publish-chartpress-images/publish-images.sh
+++ publish-chartpress-images/publish-images.sh
@@ -13,6 +13,19 @@ if [ -z "$CHARTPRESS_SPEC_DIR" ]; then
   CHARTPRESS_SPEC_DIR="."
 fi
 
+PLATFORM_ARGS=""
+BUILDER_ARG=""
+if [ ! -z "$PLATFORMS" ]; then
+  # setting up docker-buildx for multi-platform builds
+  docker buildx create --name multiarch --use
+  docker buildx inspect --bootstrap
+
+  for platform in $(echo $PLATFORMS | tr ',' ' '); do
+    PLATFORM_ARGS="$PLATFORM_ARGS --platform $platform"
+  done
+  BUILDER_ARG="--builder docker-buildx"
+fi
+
 # log in to docker
 echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
 
@@ -20,9 +33,9 @@ echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
 git config --global --add safe.directory $PWD
 # build and push the chart and images
 cd $CHARTPRESS_SPEC_DIR
-chartpress --push $CHART_TAG $IMAGE_PREFIX
+chartpress --push $CHART_TAG $IMAGE_PREFIX $PLATFORM_ARGS $BUILDER_ARG
 
 if [ ! -z "$PUSH_LATEST" ]; then
     echo "Pushing images with 'latest' tags"
-    chartpress --push --tag latest $IMAGE_PREFIX
+    chartpress --push --tag latest $IMAGE_PREFIX $PLATFORM_ARGS $BUILDER_ARG
 fi
